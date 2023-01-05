01ffab417f035fdd3ae97a880aae8e03a62ff385
Erhan Ertem <eertem@gmail.com>
2023-01-06 01:36:30 +0300

    Display a message for categories with no entries

--- src/App.js
+++ src/App.js
@@ -296,6 +296,15 @@ function CategoryFilter({ setCurrentCategory }) {
 
 // FACTLIST COMPONENT
 function FactList({ facts }) {
+	// if (facts.length === 0) {
+	if (facts.length === 0) {
+		return (
+			<p className="message">
+				No facts for this category yet! Create the first one 😋
+			</p>
+		);
+	}
+
 	return (
 		<section>
 			<ul className="facts-list">
