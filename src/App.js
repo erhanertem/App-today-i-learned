5af564f400af3ef511456a2acc2b28b19e585eee
Erhan Ertem <eertem@gmail.com>
2023-01-07 15:17:18 +0300

    Add error handling conditional to submit process

--- src/App.js
+++ src/App.js
@@ -226,8 +226,8 @@ function NewFactForm({ setShowForm, setFacts }) {
 				.select(); //NOTE: from() & insert() are already enough for supoabase to register the data however we also want to receive the new datat created in spabase back to persist in our UI so we use select() supabase method
 			// console.log(newFact);
 			setIsUploading(false);
-			//->#4.Add the new fact to the UI: add the fact to react state
-			setFacts((facts) => [newFact[0], ...facts]); // newFact data is returned as array with single entry which is why we use [0]
+			//->#4.Add the new fact to the UI: add the fact to react state unless no error
+			if (!error) setFacts((facts) => [newFact[0], ...facts]); // newFact data is returned as array with single entry which is why we use [0]
 			//->#5.Reset input fields to empty
 			setText("");
 			setSource("");
